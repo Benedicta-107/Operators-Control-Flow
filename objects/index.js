02c4fd9364af974b12f677e126202a1f30c56658
Benedicta Ibeawuchi <benedictaibeawuchi@gmail.com>
2023-02-25 19:22:23 +0100

    Added object MyInstagramPost

--- objects/index.js
+++ objects/index.js
@@ -26,4 +26,19 @@ const MyInstagramPost = new InstagramPost ("@Olumuyiya", "Peter Obi all the way,
 //Create 2 Instagram post objects from the constructor function you created above
 const ValsInstagramPost = new InstagramPost ("@Gbede", "A new season begins in Nigeria", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-h5N3a3rsH1QQFBS4QDQZSsU2kvu0LaHtdg&usqp=CAU", 350, 300);
 const ChibudosInstagramPost = new InstagramPost ("@Erifeoluwa", "Light has nothing to do with darkness", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXwSYLxme2yRTqQOWbmaKssbDxW5MyOK-PFg&usqp=CAU", 5000, 999);
- 
+
+console.log(MyInstagramPost, ValsInstagramPost, ChibudosInstagramPost);
+/**
+ * Musa Dawodu is a 19 years old Nigerian male who lives with his parents in Lekki, Lagos State. Recently, he wrote the JAMB exam and had the following scores in his combination:
+ENG - 70
+GOVT - 85
+LIT - 82
+CRK - 94
+
+Using the factory method, implement a createPerson() factory function that takes name, age and location as arguments. 
+Afterwards, create an object representing Musa from the factory function. 
+Also, implement a factory function createJambScores() that takes eng, govt, lit, crk as arguments. 
+Then create an object representing Muse’s JAMB scores. Add the object as a property to Musa object you created above in (a) above
+
+ */
+
