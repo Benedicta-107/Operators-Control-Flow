6f29125c91811c7b10ad9970a62489b1eda872f0
Benedicta Ibeawuchi <benedictaibeawuchi@gmail.com>
2023-03-21 01:24:26 +0100

    solutions to the todo list

---
+++ solutions/index.js
@@ -0,0 +1,40 @@
+// Create a simple todo list that has the following functionalities
+
+/**
+ * add a new item to the todo list
+ * remove an item in the todo list
+ * 
+    Some further requirements:
+    - the input field must be reset when the add button is clicked on 
+    - the todo list must show the most recent first
+
+    var divs = document.querySelectorAll("div");
+
+ */
+/**
+ * function modifyText() {
+  const t2 = document.getElementById("t2");
+  const isNodeThree = t2.firstChild.nodeValue === "three";
+  t2.firstChild.nodeValue = isNodeThree ? "two" : "three";
+}
+
+// Add event listener to table
+const el = document.getElementById("outside");
+el.addEventListener("click", modifyText, false);
+ */
+
+  
+
+    let todoList = document.querySelectorAll("p");
+
+    function addItem (item) {
+        
+    }
+    
+    function editItem (item) {
+    
+    }
+    
+    function deleteItem (item) {
+    
+    }
