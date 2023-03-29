075100e1545e866255d6f5194cdc4f0270c78109
Benedicta Ibeawuchi <benedictaibeawuchi@gmail.com>
2023-03-29 16:57:39 +0100

    todo list

--- solutions/index.js
+++ solutions/index.js
@@ -1,40 +1,96 @@
-// Create a simple todo list that has the following functionalities
-
-/**
- * add a new item to the todo list
- * remove an item in the todo list
- * 
-    Some further requirements:
-    - the input field must be reset when the add button is clicked on 
-    - the todo list must show the most recent first
-
-    var divs = document.querySelectorAll("div");
-
- */
-/**
- * function modifyText() {
-  const t2 = document.getElementById("t2");
-  const isNodeThree = t2.firstChild.nodeValue === "three";
-  t2.firstChild.nodeValue = isNodeThree ? "two" : "three";
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
 
-// Add event listener to table
-const el = document.getElementById("outside");
-el.addEventListener("click", modifyText, false);
- */
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
 
-  
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
 
-    let todoList = document.querySelectorAll("p");
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("new-task-container").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("incomplete-tasks").appendChild(li);
+  }
+  document.getElementById("new-task").value = "";
 
-    function addItem (item) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+// window.addEventListener('load', () => {
+//     const form = document.querySelector("#new-task-form");
+//     const input = document.querySelector("#new-task-input");
+//     const list_el = document.querySelector("#tasks");
+//     console.log(form);
+
+//     form.addEventListener('submit', (e) => {
+//         e.preventDefault();
+//         console.log("Submit form");
+
+//         const task = input.value;
+
+//         if (!task) {
+//             alert("Please fill out the task");
+//             return;
+//         }
+//         const task_el = document.createElement("div");
+//         task_el.classList.add("task");
         
-    }
-    
-    function editItem (item) {
-    
-    }
-    
-    function deleteItem (item) {
-    
-    }
+//         const task_content_el = document.createElement("div");
+//         task_content_el.classList.add("content");
+
+//         task_el.appendChild(task_content_el);
+
+//         const task_input_el = document.createElement("input");
+//         task_input_el.classList.add("text");
+//         task_input_el.type = "text";
+//         task_input_el.value = task;
+//         task_input_el.setAttribute("readonly", "readonly");
+
+//         task_content_el.appendChild(task_input_el);
+
+//         const task_actions_el = document.createElement("div");
+//         task_actions_el.classList.add("actions");
+
+//         //const task_edit_el = document
+
+//         input.value = "";
+        
+//     })
+// })
